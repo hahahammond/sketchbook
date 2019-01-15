232ad64e492f8d1d9ed48ca531289203ca0828dd
Kayla Hammond Larkin <larkink@BUMC-dot1x-155-41-166-226.bumc.bu.edu>
2019-01-15 13:07:44 -0500

    Updated script and data files

--- hiv/hiv.py
+++ hiv/hiv.py
@@ -6,9 +6,9 @@ Created on Fri Jan 11 14:04:01 2019
 @author: larkink
 """
 # Import libraries
-import pandas as pd
 import numpy as np
-import matplotlib as plt
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load data
 # Data source: http://apps.who.int/gho/data/node.main.HIVINCIDENCE?lang=en
@@ -16,6 +16,9 @@ new_infections_df = pd.read_csv('data/new_infections.csv')
 
 # Data source: http://apps.who.int/gho/data/view.main.23300
 therapy_coverage_df = pd.read_csv('data/therapy_coverage.csv')
+
+# Data source: https://data.worldbank.org/indicator/NY.GDP.MKTP.CD?view=map
+gdp_df= pd.read_csv('data/gdp.csv')
 
 # Subset data based on column values
 country_col = new_infections_df['Country']
@@ -26,11 +29,13 @@ africa_group = regions.get_group('Africa')
 new_infections_df['WHO region'] == 'Europe'
 europe_group = new_infections_df[new_infections_df['WHO region'] == 'Europe']
 
-all_hiv_df = therapy_coverage_df[therapy_coverage_df['Indicator'] == 'Estimated number of people (all ages) living with HIV']
-num_therapy_df = therapy_coverage_df[therapy_coverage_df['Indicator'] == 'Reported number of people receiving antiretroviral therapy']
+all_hiv_df = therapy_coverage_df[therapy_coverage_df['Indicator'] == 'Estimated number of people (all ages) living with HIV'][['Country', 'Numeric']]
+num_therapy_df = therapy_coverage_df[therapy_coverage_df['Indicator'] == 'Reported number of people receiving antiretroviral therapy'][['Country', 'Numeric']]
+
+gdp_df = gdp_df[['Country', '2017']]
 
 # Write df to json
-hiv_json = new_infections_df.to_json('hiv.json', orient='records')
+#new_infections_json = new_infections_df.to_json('output/new_infections.json', orient='records')
 
 # Create panel (i.e. a dictionary of dataframes)
 # NOTE: Panel is deprecated
@@ -87,3 +92,21 @@ high_infections.loc[high_infections.Country=='Brazil', 'Comments'] = 'Brazil is 
 
 # Subset top ten countries with most new infections
 top_infections = high_infections.sort_values('Numeric', ascending=False).iloc[:10][['Country', 'WHO region', 'Numeric']]
+
+# Rename columns
+top_infections.rename(columns={'Numeric':'Number of new HIV infections'}, inplace=True)
+all_hiv_df.rename(columns={'Numeric':'Estimated number of people (all ages) living with HIV'}, inplace=True)
+num_therapy_df.rename(columns={'Numeric':'Reported number of people receiving antiretroviral therapy'}, inplace=True)
+gdp_df.rename(columns={'2017':'GDP'}, inplace=True)
+
+# Merge dfs
+merged_df = top_infections.merge(all_hiv_df, on='Country').merge(num_therapy_df, on='Country').merge(gdp_df, how='left', on='Country')
+merged_df.count()
+
+# Fix missing 2017 GDP value for United Republic of Tanzania
+# Data source: https://data.worldbank.org/indicator/NY.GDP.MKTP.CD
+merged_df.loc[merged_df.Country=='United Republic of Tanzania', 'GDP'] = 52090320325.4736
+merged_df.count()
+
+# Visualize data
+plt.bar(merged_df.Country, merged_df.GDP)
